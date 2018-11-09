0490a75e803b177e23fc2bda9e4820e2b8c457e4
Sarah <sarah@sarahfrench.co.uk>
2018-11-09 14:21:16 +0000

    Modified pawn.js to pass second version of pawn.spec.js

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -1,5 +1,7 @@
 import Piece from './piece';
-import Player from '../../../src/engine/player';
+import Player from '../player';
+import Square from '../square';
+
 
 
 export default class Pawn extends Piece {
@@ -9,25 +11,35 @@ export default class Pawn extends Piece {
 
     getAvailableMoves(board) { //currently doesn't allow Pawn to move 2 squares on first move for that piece
       let currentSquare = board.findPiece(this);
-      let availableSquare = currentSquare;
-
-      let pieceUnmoved = 0;
-      if ((this.player === Player.WHITE) && (currentSquare.row === 2)){
-        pieceUnmoved = 1;
-      } else if ((this.player === Player.BLACK) && (currentSquare.row === 7)){
-        pieceUnmoved = 1;
-      }
-
+      let availableSquare = board.findPiece(this);
       let availableSquareList =[];
 
-      for (let i = 1; i <= 1+pieceUnmoved; i++ ){
+      console.log(availableSquare);
+
+      //Previously moved
       if (this.player === Player.WHITE){
         availableSquare.row += 1;
-        availableSquareList.push(availableSquare);
+        availableSquareList.push(new Square(availableSquare.row, availableSquare.col));
+        if (currentSquare.row === 1){
+          availableSquare.row += 1;
+          availableSquareList.push(new Square(availableSquare.row, availableSquare.col));
+        }
       } else if (this.player === Player.BLACK){
         availableSquare.row -= 1;
-        availableSquareList.push(availableSquare);
+        availableSquareList.push(new Square(availableSquare.row, availableSquare.col));
+        if (currentSquare.row === 6){
+          availableSquare.row -= 1;
+          availableSquareList.push(new Square(availableSquare.row, availableSquare.col));
         }
-      }
+        }
+
+      //      if ((this.player === Player.WHITE) && (currentSquare.row === 1)){
+
+
       return availableSquareList;
+}
+
+
+
+
 }
