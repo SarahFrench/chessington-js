2b4e795b215fcd00b72bb4b215b6daedcebb3a47
Sarah <sarah@sarahfrench.co.uk>
2018-11-09 12:22:08 +0000

    Partway working towards passing second test

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -11,11 +11,23 @@ export default class Pawn extends Piece {
       let currentSquare = board.findPiece(this);
       let availableSquare = currentSquare;
 
+      let pieceUnmoved = 0;
+      if ((this.player === Player.WHITE) && (currentSquare.row === 2)){
+        pieceUnmoved = 1;
+      } else if ((this.player === Player.BLACK) && (currentSquare.row === 7)){
+        pieceUnmoved = 1;
+      }
+
+      let availableSquareList =[];
+
+      for (let i = 1; i <= 1+pieceUnmoved; i++ ){
       if (this.player === Player.WHITE){
         availableSquare.row += 1;
+        availableSquareList.push(availableSquare);
       } else if (this.player === Player.BLACK){
         availableSquare.row -= 1;
+        availableSquareList.push(availableSquare);
+        }
       }
-      return availableSquare;
-      }
+      return availableSquareList;
 }
