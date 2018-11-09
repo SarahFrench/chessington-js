d2a8f6766023a264ff803b982e7f236f41fdeb28
Sarah <sarah@sarahfrench.co.uk>
2018-11-09 17:33:40 +0000

    Modified queen.js to pass queen.spec.js test. Changed comments on bishop.js

--- src/engine/pieces/bishop.js
+++ src/engine/pieces/bishop.js
@@ -14,30 +14,29 @@ export default class Bishop extends Piece {
 
         let row, col;
 
-        //Searching positive direction
+        //Diagonal in positive direction
         for ( let i = -7; i < 8; i++ ) { //always adding i, which takes on -+ values
           row = currentSquare.row + i;
           col = currentSquare.col + i;
           if (( 0 <= row && row <= 7) && ( 0 <= col && col <= 7)) { availableSquareList.push(new Square(row,col)) }
         }
 
-        //Searching negative direction
-
+        //Diagonal in negative direction
         for ( let i = -7; i < 8; i++ ) { //always adding i, which takes on -+ values
           row = currentSquare.row - i;
           col = currentSquare.col + i;
           if (( 0 <= row && row <= 7) && ( 0 <= col && col <= 7)) { availableSquareList.push(new Square(row,col)) }
         }
 
-        //ANOTHER METHOD FOR DOING THE ABOVE LOOP
-        // row = currentSquare.row + 8 ;
-        // col = currentSquare.col - 8;
-        //
-        // for ( let i = 0; i < 18; i++ ) {
-        //   row = row - 1;
-        //   col = col + 1;
-        //   if (( 0 <= row && row <= 7) && ( 0 <= col && col <= 7)) { availableSquareList.push(new Square(row,col)) }
-        // }
+              //ANOTHER METHOD FOR DOING THE ABOVE LOOP
+              // row = currentSquare.row + 8 ;
+              // col = currentSquare.col - 8;
+              //
+              // for ( let i = 0; i < 18; i++ ) {
+              //   row = row - 1;
+              //   col = col + 1;
+              //   if (( 0 <= row && row <= 7) && ( 0 <= col && col <= 7)) { availableSquareList.push(new Square(row,col)) }
+              // }
 
         //Remove currentSquare
         let filteredAvailableSquareList = availableSquareList.filter( square => { return !square.equals(currentSquare)})
--- src/engine/pieces/queen.js
+++ src/engine/pieces/queen.js
@@ -1,4 +1,6 @@
 import Piece from './piece';
+import Player from '../player';
+import Square from '../square';
 
 export default class Queen extends Piece {
     constructor(player) {
@@ -6,6 +8,35 @@ export default class Queen extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+
+      let currentSquare = board.findPiece(this);
+      let availableSquareList = [];
+      let row, col;
+
+      //Horizontally (fixed col)
+      for (let i = 0; i < 8; i++){
+        availableSquareList.push(new Square(i,currentSquare.col))
+      }
+      //Vertically (fixed col)
+      for (let i = 0; i < 8; i++){
+        availableSquareList.push(new Square(currentSquare.row, i))
+      }
+
+      //Diagonal in positive direction
+      for ( let i = -7; i < 8; i++ ) { //always adding i, which takes on -+ values
+        row = currentSquare.row + i;
+        col = currentSquare.col + i;
+        if (( 0 <= row && row <= 7) && ( 0 <= col && col <= 7)) { availableSquareList.push(new Square(row,col)) }
+      }
+
+      //Diagonal in negative direction
+      for ( let i = -7; i < 8; i++ ) { //always adding i, which takes on -+ values
+        row = currentSquare.row - i;
+        col = currentSquare.col + i;
+        if (( 0 <= row && row <= 7) && ( 0 <= col && col <= 7)) { availableSquareList.push(new Square(row,col)) }
+      }
+
+      let filteredAvailableSquareList = availableSquareList.filter( square => { return !square.equals(currentSquare)})
+      return filteredAvailableSquareList;
     }
 }
